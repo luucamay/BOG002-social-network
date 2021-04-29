4032d9072143dacbbfe87d96c1eb09eac8558a4b
Lupe <lupemaydana@gmail.com>
2021-04-29 18:22:13 -0400

    Add test mocked with a library

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,26 @@
 // importamos la funcion que vamos a testear
 import { login } from '../src/lib/index';
+const { mockFirebase } = require('firestore-jest-mock');
+
+// Create a fake Firestore with a `users` and `posts` collection
+mockFirebase({
+  database: {
+    users: [
+      { id: 'abc123', name: 'Homer Simpson' },
+      { id: 'abc456', name: 'Lisa Simpson' },
+    ],
+    posts: [{ id: '123abc', title: 'Really cool title' }],
+  },
+});
+
+global.firebase = require("firebase")
+//console.log(firebase);
 
 describe('login', () => {
   it('debería ser una función', () => {
     expect(typeof login).toBe('function');
   });
   it('debe loguear', () => {
-
+    return login("aaaa", "bbbb").then(console.log)
   })
 });
