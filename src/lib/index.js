d72195c1b464fc1eb2f2cc06e28a5c7c92abeb12
Lupe <lupemaydana@gmail.com>
2021-05-05 11:03:00 -0400

    test de autentication

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,5 +5,32 @@ export const myFunction = () => {
   console.log('Hola mundo!');
 };
 
-export const login = (email, password) => firebase.auth()
-  .createUserWithEmailAndPassword(email, password);
+export const login = (email, password) => {
+
+  return firebase.auth()
+    .signInWithEmailAndPassword(email, password);
+}
+
+
+export const register = (email, password) => {
+  return firebase.auth()
+    .createUserWithEmailAndPassword(email, password);
+}
+
+
+
+// ----------------------------FUNCION ELIMINAR POST---------------------------->
+export const deletePost = (uid) => {
+  return firebase.firestore()
+    .collection('publicaciones').doc(uid).delete();
+}
+
+// ----------------------------FUNCION AGREGAR DATOS POST--------------------------->
+
+export const addPostsData = (user, textPublication) => {
+  return firebase.firestore().collection('publicaciones').add({
+    date: new Date().toLocaleString(),
+    text: textPublication.value,
+    uid: user.uid
+  })
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,26 +1,33 @@
 // importamos la funcion que vamos a testear
-import { login } from '../src/lib/index';
-const { mockFirebase } = require('firestore-jest-mock');
+import { login, register, crearUsuario } from '../src/lib/index';
 
-// Create a fake Firestore with a `users` and `posts` collection
-mockFirebase({
-  database: {
-    users: [
-      { id: 'abc123', name: 'Homer Simpson' },
-      { id: 'abc456', name: 'Lisa Simpson' },
-    ],
-    posts: [{ id: '123abc', title: 'Really cool title' }],
+const firebasemock = require('firebase-mock');
+const mockauth = new firebasemock.MockAuthentication();
+const mocksdk = new firebasemock.MockFirebaseSdk(
+  () => {
+    return null;
   },
-});
+  () => {
+    return mockauth;
+  }
+);
+mockauth.autoFlush();
+global.firebase = mocksdk;
 
-global.firebase = require("firebase")
-//console.log(firebase);
+//global.firebase = firebasemock.MockFirebaseSdk(() => null, () => mockauth,);
 
-describe('login', () => {
-  it('debería ser una función', () => {
-    expect(typeof login).toBe('function');
-  });
-  it('debe loguear', () => {
-    return login("aaaa", "bbbb").then(console.log)
+
+describe('register', () => {
+
+  test('deberia de loguearme', () => {
+    const promesa = register('ben@example.com', 'examplePass');
+
+    return promesa
+      .then((user) => {
+        //console.log(user);
+        expect(typeof user).toBe("object");
+        expect(user.email).toBe("ben@example.com")
+      })
+
   })
 });
